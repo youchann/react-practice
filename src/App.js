c9fd60a99ffb4d3e8881456ad7680e39229d9237
yutaro1031 <yutaro50@gmail.com>
2019-03-28 20:04:59 +0900

    関数コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div>Hello World!!</div>;
-  }
-  // jsxを使わない場合
-  // render() {
-  //   return React.createElement(
-  //     "div",
-  //     null,
-  //     "Hello World!!"
-  //   )
-  // }
+// ↓クラスコンポーネント
+// class App extends Component {
+//   render() {
+//     return <div>Hello World!!</div>;
+//   }
+
+//   // classはjsの予約語なので、classNameに置き換えて、DOMを書く
+
+//   // render() {
+//   //   const greeting = "Hi!";
+//   //   const dom = <div className="foo">{greeting}</div>;
+//   //   return dom;
+//   // }
+
+//   // jsxを使わない場合
+//   // render() {
+//   //   return React.createElement(
+//   //     "div",
+//   //     null,
+//   //     "Hello World!!"
+//   //   )
+//   // }
+// }
+
+// 関数コンポーネント
+const App = () => {
+  return (
+    <div>
+      <Cat/>
+      <Cat/>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>meou!</div>
 }
 
 export default App;
