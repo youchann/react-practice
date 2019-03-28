932fa92b5285532ed9a148efa6a8f3e06b4d8416
yutaro1031 <yutaro50@gmail.com>
2019-03-28 20:24:03 +0900

    props

--- src/App.js
+++ src/App.js
@@ -26,16 +26,27 @@ import React from 'react';
 
 // 関数コンポーネント
 const App = () => {
+  const profiles = [
+    { name: "taro", age: 10 },
+    { name: "hanako", age: 5 },
+    { name: "hanao" }
+  ]
   return (
-    <div>
-      <Cat/>
-      <Cat/>
-    </div>
+    // <div>
+    //   <User name={"tarou"} age={10}/>
+    // </div>
+    profiles.map((profile, index) => {
+      return <User name={profile.name} age={profile.age} key={index}/>
+    })
   )
 }
 
-const Cat = () => {
-  return <div>meou!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}! and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
