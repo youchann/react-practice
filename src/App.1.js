44d44fe61f200aee3623b8a0aaf802a2abf45487
yutaro1031 <yutaro50@gmail.com>
2019-03-29 14:10:55 +0900

    state

---
+++ src/App.1.js
@@ -0,0 +1,58 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// ↓クラスコンポーネント
+// class App extends Component {
+//   render() {
+//     return <div>Hello World!!</div>;
+//   }
+
+//   // classはjsの予約語なので、classNameに置き換えて、DOMを書く
+
+//   // render() {
+//   //   const greeting = "Hi!";
+//   //   const dom = <div className="foo">{greeting}</div>;
+//   //   return dom;
+//   // }
+
+//   // jsxを使わない場合
+//   // render() {
+//   //   return React.createElement(
+//   //     "div",
+//   //     null,
+//   //     "Hello World!!"
+//   //   )
+//   // }
+// }
+
+// 関数コンポーネント
+const App = () => {
+  // const profiles = [
+  //   { name: "taro", age: 10 },
+  //   { name: "hanako", age: 5 },
+  //   { name: "string" }
+  // ]
+  return (
+    // <div>
+    //   <User name={"tarou"} age={10}/>
+    // </div>
+    profiles.map((profile, index) => {
+      return <User name={profile.name} age={profile.age} key={index}/>
+    })
+  )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}! and {props.age} years old!</div>
+}
+
+// User.defaultProps = {
+//   age: 1
+// }
+// // 型を定義する
+// User.propTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number.isRequired
+// }
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,58 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-// ↓クラスコンポーネント
-// class App extends Component {
-//   render() {
-//     return <div>Hello World!!</div>;
-//   }
-
-//   // classはjsの予約語なので、classNameに置き換えて、DOMを書く
-
-//   // render() {
-//   //   const greeting = "Hi!";
-//   //   const dom = <div className="foo">{greeting}</div>;
-//   //   return dom;
-//   // }
-
-//   // jsxを使わない場合
-//   // render() {
-//   //   return React.createElement(
-//   //     "div",
-//   //     null,
-//   //     "Hello World!!"
-//   //   )
-//   // }
-// }
+import React, { Component } from 'react';
 
 // 関数コンポーネント
-const App = () => {
-  const profiles = [
-    { name: "taro", age: 10 },
-    { name: "hanako", age: 5 },
-    { name: "string" }
-  ]
-  return (
-    // <div>
-    //   <User name={"tarou"} age={10}/>
-    // </div>
-    profiles.map((profile, index) => {
-      return <User name={profile.name} age={profile.age} key={index}/>
-    })
-  )
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}! and {props.age} years old!</div>
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
 
-// User.defaultProps = {
-//   age: 1
-// }
-// // 型を定義する
-// User.propTypes = {
-//   name: PropTypes.string,
-//   age: PropTypes.number.isRequired
-// }
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
+}
 
 export default App;
