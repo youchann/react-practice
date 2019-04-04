181f5d6708320667fc1cdd37878e51137ee1a8d4
yutaro1031 <yutaro50@gmail.com>
2019-04-04 14:43:20 +0900

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,17 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import {
+  READ_EVENTS,
+  DELETE_EVENT,
+} from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       // 送られてきたjsonをそれぞれのidをキーとした配列に格納
       return _.mapKeys(action.response.data, 'id')
+    case DELETE_EVENT:
+      delete events[action.id]
+      return { ...events }
     default:
       return events
   }
